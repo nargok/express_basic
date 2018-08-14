8aa1db5a34e1cf6c38f0cbc44ea7b095f7aa2ea7
Ryoichi Gokan <nargok@gmail.com>
2018-08-14 16:18:29 +0900

    use query parameter

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   var msg = 'This is Express page!<br>' + 
             'これは、トップページです';
+  var url = '/other?name=taro&pass=yamada'
   res.render('index.ejs', {
     title: 'Index',
     content: msg,
-    link:{href: '/other', text:'※別のページに移動'}
+    link:{href: url, text:'※別のページに移動'}
   });
 });
 
 app.get('/other', (req, res) => {
-  var msg = 'This is other page!<br>' + 
-            'これは、用意された別のページです';
+  var name = req.query.name;
+  var pass = req.query.pass;
+  var msg = 'あなたの名前は「' + name +
+            '」<br>パスワードは「' + pass + '」です。';
   res.render('index.ejs', {
     title: 'other',
     content: msg,
