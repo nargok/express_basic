56e7e747fc6b9fcb36e4d850985c9467d576fa17
Ryoichi Gokan <nargok@gmail.com>
2018-08-14 16:09:27 +0900

    link to other page

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   var msg = 'This is Express page!<br>' + 
-            'これは、スタイルシートを利用した例です。';
+            'これは、トップページです';
   res.render('index.ejs', {
     title: 'Index',
     content: msg,
+    link:{href: '/other', text:'※別のページに移動'}
+  });
+});
+
+app.get('/other', (req, res) => {
+  var msg = 'This is other page!<br>' + 
+            'これは、用意された別のページです';
+  res.render('index.ejs', {
+    title: 'other',
+    content: msg,
+    link:{href:'/', text:'※トップページに戻る'}
   });
 });
 
