f817ae31f455a3094f1c92d0b59fe1566b67a43b
Ryoichi Gokan <nargok@gmail.com>
2018-08-14 15:59:10 +0900

    user ejs as engine

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var ejs = require('ejs');
+
 var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Express!')
-})
+app.engine('ejs', ejs.renderFile);
 
-app.listen(3000, () => {
-  console.log('Start server port:3000')
-})
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    title: 'Index',
+    content: 'This is Express-app Top page!'
+  });
+});
+
+var server = app.listen(3000, () => {
+  console.log('server is running!');
+});
